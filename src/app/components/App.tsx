3d71435ac36e10dbc6d6f8026411dd4cfc0b5a13
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-01-29 23:58:08 +0900

    add styled components

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,4 +1,4 @@
-import Header from './Header';
+import Header from "./Header";
 
 const App = ({ children }: { children?: any }) => (
   <main>
--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,7 +1,25 @@
-import App from '../components/App';
+import styled, { css } from "styled-components";
+import App from "../components/App";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solit palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${(props) =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: wite;
+    `}
+`;
 
 export default () => (
   <App>
     <p>Index Page</p>
+    <Button>Button</Button>
   </App>
 );
